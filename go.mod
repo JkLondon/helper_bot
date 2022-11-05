b2ed1cb8e03d1ef037605e1795ad3c43b4c4c4fd
jackl <mikheev.ii@phystech.edu>
2022-11-06 01:23:42 +0300

    added tg part

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module weatherEveryDay
 go 1.19
 
 require github.com/joho/godotenv v1.4.0
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"strings"
 	"weatherEveryDay/httpClient"
 	"weatherEveryDay/templates"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -17,13 +18,43 @@ func init() {
 	}
 }
 func main() {
-	var apiKey = os.Getenv("WEATHER_API_KEY")
-	weatherForecast, err := httpClient.GetWeather(apiKey)
+	var (
+		apiKey        = os.Getenv("WEATHER_API_KEY")
+		telegramToken = os.Getenv("TELEGRAM_APITOKEN")
+	)
+
+	bot, err := tgbotapi.NewBotAPI(telegramToken)
 	if err != nil {
-		log.Fatal(err.Error())
-		return
+		panic(err)
 	}
-	weather := weatherForecast.Data[0]
-	message := templates.MakeTGWeatherMessage(weather)
-	fmt.Println(message)
+
+	bot.Debug = true
+
+	updateConfig := tgbotapi.NewUpdate(0)
+
+	updateConfig.Timeout = 30
+
+	// Start polling Telegram for updates.
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		switch strings.ToLower(update.Message.Text) {
+		case "/weather":
+			weatherForecast, err := httpClient.GetWeather(apiKey)
+			if err != nil {
+				log.Fatal(err.Error())
+				return
+			}
+			weather := weatherForecast.Data[0]
+			message := templates.MakeTGWeatherMessage(weather)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, message)
+			msg.ReplyToMessageID = update.Message.MessageID
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
+		}
+	}
 }
