b044ce90e805af66e22c2a6296429f722ba7c4bd
JLondon <mikheev.ii@phystech.edu>
2023-02-04 18:17:15 +0300

    experimenting with *

--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module weatherEveryDay
 
 go 1.19
 
-require github.com/joho/godotenv v1.4.0
-
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
-	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.4.0
+	github.com/shopspring/decimal v1.3.1
 )
--- templates/jiraTemplates.go
+++ templates/jiraTemplates.go
@@ -2,8 +2,8 @@ package templates
 
 const (
 	ReportIntro        = "Отчёт по джире за %s:\n"
-	TaskDoneMonthWeek  = "За месяц было выполнено **%d** задач, за неделю — **%d**\n"
-	WeekMemberActivity = "\nЗа эту неделю **%d** " +
+	TaskDoneMonthWeek  = "За месяц было выполнено ***%d*** задач, за неделю — *%d*\n"
+	WeekMemberActivity = "\nЗа эту неделю ****%d**** " +
 		"cотрудников выполнили определенное количество задач, а именно:\n"
 	MemberReport        = "**%s** выполнил(а) **%d** задач из них передоговоров **%d**\n"
 	MonthMemberActivity = "\nЗа этот месяц **%d** " +
